93aa03b51769f5fe874e7d7d8d722e20ebd62282
dogan <dogan@DOGAN-PC>
2022-02-03 16:20:55 +0300

    Add project files.

---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Customer :IEntity
+    {
+        public string? CustomerId { get; set; }
+        public string? ContactName { get; set; }
+        public string? ConpanyName { get; set; }
+        public string? Ctiy { get; set; }
+
+    }
+}
